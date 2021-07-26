3e00b2c300f012305cd752e9b2103f5089445597
alenj <alenjojo252001@gmail.com>
2021-07-26 14:32:41 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCsfF_8Y8YetlXaV9Dx3T_wfTH0S9P49g",
+    authDomain: "clone-f15f1.firebaseapp.com",
+    projectId: "clone-f15f1",
+    storageBucket: "clone-f15f1.appspot.com",
+    messagingSenderId: "295577712497",
+    appId: "1:295577712497:web:f682087712de75dee328d0",
+    measurementId: "G-9NG7DES1FZ"
+};
