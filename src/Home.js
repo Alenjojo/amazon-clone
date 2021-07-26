686912928e6e417b1229ac36245a4b84b4cdf88a
alenj <alenjojo252001@gmail.com>
2021-07-26 16:33:40 +0530

    Added Home file

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img className="home_image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/primeday/PD18/AAFeedback/Bruno/1500x300_Starts_Banner_v2._CB474351192_.gif"
+                    alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
