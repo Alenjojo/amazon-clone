7526fa1940cc36da5b85d94700678f1a1a404253
Alen Jojo <alen.j@menerva.tech>
2022-10-26 10:11:16 +0530

    dynamic content

--- src/Home.js
+++ src/Home.js
@@ -6,95 +6,89 @@ import { useStateValue } from "./StateProvider";
 
 function Home() {
   const [items, setItems] = useState([]);
+  const [banners, setBanner] = useState([]);
   const [{ user }] = useStateValue();
 
   useEffect(() => {
     // db.collection("items").doc().set(
     //   {
-    //     name: "Quaker Oats",
-    //     price: "2.19",
-    //     rating: "4.7",
-    //     img: "https://m.media-amazon.com/images/I/71JRvM2PITL._AC_UL480_QL65_.jpg",
+    //     name: "Acer Predator X35 1800R Curved 35 Inch UltraWide QHD Gaming Monitor I G-SYNC Ultimate I Quantum Dot I 200Hz",
+    //     price: "89999",
+    //     rating: "5",
+    //     img: "https://m.media-amazon.com/images/I/61PG4UvgJxS._AC_UL480_QL65_.jpg",
     //   },
     //   []
     // );
-
-    getItems();
+    if (user) {
+      getItems();
+      getBanner();
+    } else {
+      setItems([]);
+      setBanner([]);
+    }
   }, [user]);
 
   const getItems = () => {
-    if (user) {
-      db.collection("items").onSnapshot((snapshot) =>
+    db.collection("items")
+      .orderBy("price", "asc")
+      .onSnapshot((snapshot) =>
         setItems(
           snapshot.docs.map((doc) => ({
             data: doc.data(),
           }))
         )
       );
-      console.log(items);
-    } else {
-      setItems([]);
-    }
+  };
+
+  const getBanner = () => {
+    db.collection("banner").onSnapshot((snapshot) =>
+      setBanner(
+        snapshot.docs.map((doc) => ({
+          data: doc.data(),
+        }))
+      )
+    );
   };
 
   return (
     <div className="home">
       <div className="home_container">
-        <img
-          className="home_image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Events/Pug/launches21/1500x600_shop-now._CB663404287_.jpg"
-          alt=""
-        />
+        {banners?.map((banner) => (
+          <img className="home_image" src={banner.data.img} alt="" />
+        ))}
 
         <div className="home_row">
-          <Product
-            id={0}
-            title="Echo (4th Gen, 2020 release) | Premium sound powered by Dolby and Alexa (Black)"
-            price={15.92}
-            image="https://m.media-amazon.com/images/I/61-DjUz7JxL._AC_UY327_QL65_.jpg"
-            rating={3}
-          />
-          <Product
-            id={1}
-            title="ASUS ROG Strix G17 (2021), 17.3-inch (43.94 cms) FHD 144Hz, AMD Ryzen 7 5800H, GeForce RTX 3050 4GB Graphics, Gaming Laptop (8GB/1TB SSD/Windows 10/Original Black/2.4 Kg), G713QC-HX052T"
-            price={499.19}
-            image="https://m.media-amazon.com/images/I/71fkB1F3cVL._AC_UY327_QL65_.jpg"
-            rating={5}
-          />
+          {items?.slice(0, 2).map((item) => (
+            <Product
+              id={item.data.id}
+              title={item.data.name}
+              price={item.data.price}
+              image={item.data.img}
+              rating={item.data.rating}
+            />
+          ))}
         </div>
         <div className="home_row">
-          <Product
-            id={2}
-            title="Fitbit Inspire HR Health and Fitness Tracker with Heart Rate (Black)"
-            price={12.99}
-            image="https://m.media-amazon.com/images/I/61fPdr7lutL._AC_UY327_QL65_.jpg"
-            rating={4}
-          />
-          <Product
-            id={3}
-            title="Surf Excel Matic Front Load Liquid Detergent Refill Pouch -Super Saver Pack
-                         Designed For 100% Tough Stain Removal In Front load Machines, 2 L"
-            price={22.7}
-            image="https://m.media-amazon.com/images/I/71AfKPJFjoL._AC_UL480_QL65_.jpg"
-            rating={5}
-          />
-          <Product
-            id={4}
-            title="Quaker Oats Pouch, 1000 g"
-            price={32.9}
-            image="https://m.media-amazon.com/images/I/71JRvM2PITL._AC_UL480_QL65_.jpg"
-            rating={2}
-          />
+          {items?.slice(2, 5).map((item) => (
+            <Product
+              id={item.data.id}
+              title={item.data.name}
+              price={item.data.price}
+              image={item.data.img}
+              rating={item.data.rating}
+            />
+          ))}
         </div>
         <div className="home_row">
-          <Product
-            id={5}
-            title="Acer Predator X35 1800R Curved 35 Inch UltraWide QHD Gaming Monitor I G-SYNC Ultimate
-                         I Quantum Dot I 200Hz I VESA Display HDR 1000 I Adjustable Stand I Display Port, HDMI Port & USB 3.0 HUB "
-            price={72.4}
-            image="https://m.media-amazon.com/images/I/61PG4UvgJxS._AC_UL480_QL65_.jpg"
-            rating={3}
-          />
+          {items?.slice(5, 6).map((item) => (
+            <Product
+              id={item.data.id}
+              title={item.data.name}
+              price={item.data.price}
+              image={item.data.img}
+              rating={item.data.rating}
+            />
+          ))}
         </div>
       </div>
     </div>
