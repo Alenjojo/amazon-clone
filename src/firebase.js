2b1867c6899a951e3d1a3a3ee71cf414da323306
Alen Jojo <alenjojo252001@gmail.com>
2022-11-26 16:34:02 +0530

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBCsfF_8Y8YetlXaV9Dx3T_wfTH0S9P49g",
-    authDomain: "clone-f15f1.firebaseapp.com",
-    projectId: "clone-f15f1",
-    storageBucket: "clone-f15f1.appspot.com",
-    messagingSenderId: "295577712497",
-    appId: "1:295577712497:web:f682087712de75dee328d0",
-    measurementId: "G-9NG7DES1FZ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
